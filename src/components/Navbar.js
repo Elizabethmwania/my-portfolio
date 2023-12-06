4ed1281978031ba53ff96858f1a31aac40cd995e
Elizabethmwania <elizabethmwania257@gmail.com>
2023-12-06 16:55:30 +0300

    styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,20 @@ import { ArrowRightIcon  } from '@heroicons/react/solid'
 import passport from'../images/profile.jpeg';
 const Navbar = () => {
     return (
-        <header className="bg-orange-600 md:sticky top-0 z-10">
+        <header className="bg-gray-900 md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a href="#"
                 className="pasport-img">
                 <img className='passport' src= {passport} />
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-                <a href="#projects" className="mr-5 hover:text-white">
+                <a href="#projects" className="text-white mr-5 hover:text-orange-600">
                     Projects
                 </a>
-                <a href="#skills" className="mr-5 hover:text-white">
+                <a href="#skills" className="text-white mr-5 hover:text-orange-600">
                     Skills
                 </a>
-                <a href="#contact" className="mr-5 hover:text-white">
+                <a href="#contact" className="text-white mr-5 hover:text-orange-600">
                     Contact
                 </a>
                 </nav>
